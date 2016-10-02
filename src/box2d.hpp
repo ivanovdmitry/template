21d5eab2512b32e6a7e0adddf413e0d56a9a3d9a
Dmitry Ivanov <ivanovdmitrysrgeevich8@gmail.com>
2016-10-03 00:37:55 +0300

    use shared_ptr

--- src/box2d.hpp
+++ src/box2d.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+
 #include "point2d.hpp"
 
 
@@ -7,7 +9,7 @@ class Box2D
 {
 public:
     Box2D() = default;
-    Box2D(Point2D in_min, Point2D in_max) : m_min(in_min), m_max(in_max) {}
+    Box2D(std::shared_ptr<Point2D> & in_min, std::shared_ptr<Point2D> & in_max) : m_min(in_min), m_max(in_max) {}
 
     Box2D(Box2D const & obj) : m_min(obj.m_min), m_max(obj.m_max) {}
 
@@ -18,8 +20,8 @@ public:
 
     bool Crossing(Box2D const & box) const;
 
-    Point2D & GetMin() { return m_min; }
-    Point2D & GetMax() { return m_max; }
+    std::shared_ptr<Point2D> & GetMin() { return m_min; }
+    std::shared_ptr<Point2D> & GetMax() { return m_max; }
 
 
     bool operator == (Box2D const & obj) const
@@ -37,11 +39,12 @@ public:
 
     friend std::ostream & operator << (std::ostream & os, Box2D const & obj)
     {
-        os << "Box2D {(" << obj.m_min.x() << ", " << obj.m_min.y() << "), (" << obj.m_max.x() << ", " << obj.m_max.y() << ")}";
+        os << "Box2D {(" << obj.m_min->x() << ", " << obj.m_min->y() << "), (" << obj.m_max->x() << ", " << obj.m_max->y() << ")}";
         return os;
     }
 
 private:
-    Point2D m_min, m_max;
+    std::shared_ptr<Point2D> m_min = std::make_shared<Point2D>();
+    std::shared_ptr<Point2D> m_max = std::make_shared<Point2D>();
 
 };
--- tests/box2d_test.cpp
+++ tests/box2d_test.cpp
@@ -1,20 +1,22 @@
 #include "gtest/gtest.h"
 #include <sstream>
 #include <unordered_set>
-
+#include <memory>
 #include "box2d.hpp"
 
 
 TEST(box2d_test, test_construction)
 {
   Box2D box1;
-  EXPECT_EQ(box1.GetMin(), Point2D(0.0f, 0.0f));
-  EXPECT_EQ(box1.GetMax(), Point2D(0.0f, 0.0f));
+  EXPECT_EQ(*box1.GetMin(), *std::make_shared<Point2D>(0.0f, 0.0f));
+  EXPECT_EQ(*box1.GetMax(), *std::make_shared<Point2D>(0.0f, 0.0f));
 
-  Point2D point1(1.1f,1.2f), point2(2.1f,2.2f);
+  auto point1 = std::make_shared<Point2D>(1.1f,1.2f);
+  auto point2 = std::make_shared<Point2D>(2.1f,2.2f);
   Box2D box2 (point1, point2);
-  EXPECT_EQ(box2.GetMin(), Point2D(1.1,1.2));
-  EXPECT_EQ(box2.GetMax(), Point2D(2.1f,2.2f));
+
+  EXPECT_EQ(*box2.GetMin(), *std::make_shared<Point2D>(1.1,1.2));
+  EXPECT_EQ(*box2.GetMax(), *std::make_shared<Point2D>(2.1f,2.2f));
 
   Box2D box3 = box2;
   EXPECT_EQ(box3, box2);
@@ -23,7 +25,8 @@ TEST(box2d_test, test_construction)
 
 TEST(box2d_test, test_output)
 {
-  Point2D point1(1.1f,1.2f), point2(2.1f,2.2f);
+  auto point1 = std::make_shared<Point2D>(1.1f,1.2f);
+  auto point2 = std::make_shared<Point2D>(2.1f,2.2f);
 
   std::stringstream s;
   s << Box2D(point1, point2);
