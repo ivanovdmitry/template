144e94624f6519d8c423f17f16b089d60e1ef88c
Dmitry Ivanov <ivanovdmitrysrgeevich8@gmail.com>
2016-10-18 13:41:02 +0300

    fix codestyle

--- src/Logger.hpp
+++ src/Logger.hpp
@@ -12,10 +12,6 @@ class Logger
 {
 public:
 
-  static const std::string m_LogLevelDebug;
-  static const std::string m_LogLevelInfo;
-  static const std::string m_LogLevelError;
-
   void log(const std::string& message, const std::string& logLevel)
   {
     m_outputStream.open(m_FileName, std::ios_base::app);
@@ -23,6 +19,10 @@ public:
     m_outputStream.close();
   }
 
+  static const std::string m_LogLevelDebug;
+  static const std::string m_LogLevelInfo;
+  static const std::string m_LogLevelError;
+  
 protected:
   std::ofstream m_outputStream;
   static const char* const m_FileName;
