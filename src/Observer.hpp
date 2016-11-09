523fa96895f27db4890395e82b3f522418f70f07
Dmitry Ivanov <ivanovdmitrysrgeevich8@gmail.com>
2016-11-10 00:22:00 +0300

    added a simple class observer

--- src/Observer.hpp
+++ src/Observer.hpp
@@ -1,2 +1,52 @@
 #pragma once
 
+#include <functional>
+#include <list>
+#include <algorithm>
+#include <memory>
+#include <string>
+
+using MessageHandler = std::function<void()>;
+
+class IObserver // Subscriber
+{
+public:
+  virtual ~IObserver() {}
+  virtual void SendMessage(MessageHandler) = 0;
+  virtual bool operator==(IObserver &) = 0;
+  virtual std::size_t hash() = 0;
+};
+
+using ObserverPtr = std::shared_ptr<IObserver>;
+
+class Dispatcher
+{
+public:
+
+  void Subscribe(ObserverPtr const & newSubscriber)
+  {
+    for (auto it = std::begin(m_subscriberList); it != std::end(m_subscriberList); ++it)
+    {
+      if (*it == newSubscriber)
+        return;
+    }
+    m_subscriberList.push_back(newSubscriber);
+  }
+
+  void Unsubscribe(ObserverPtr const & subscriber)
+  {
+    auto it = std::find(m_subscriberList.begin(), m_subscriberList.end(), subscriber);
+    if (it != m_subscriberList.end())
+      m_subscriberList.erase(it);
+  }
+
+  void SendMessage(MessageHandler const & msg)
+  {
+    for (auto const i : m_subscriberList)
+      i->SendMessage(msg);
+  }
+
+protected:
+  std::list<ObserverPtr> m_subscriberList;
+};
+
