57f1a0caa61de11bf35818c1497e22c9d8e6d60b
Dmitry Ivanov <ivanovdmitrysrgeevich8@gmail.com>
2016-11-23 01:02:50 +0300

    smooth flashing

--- src/Star.hpp
+++ src/Star.hpp
@@ -15,29 +15,35 @@ public:
   {
     std::srand(unsigned(std::clock()));
     unsigned char size = std::rand() % 8;
-    m_size = QSize(size, size);
-    m_sizeTmp = size;
+    m_sizeTmp = QSize(size, size);
+    m_size = size;
+    nu = static_cast<float> (std::rand() % 2);
+    fi = (static_cast<float> (std::rand() % 1000))/1000.0;
     
     m_position = QVector2D(std::rand() % 800, std::rand() % 600);
   }
 
-  void Update()
+  void Update(float elapsedSeconds)
   {
-    unsigned char size = m_size.width();
-    // then change the size
-    if (size == 0)
-      size = m_sizeTmp;
-    else
-      size = 0;
+    unsigned char size = m_sizeTmp.width();
 
-    m_size = QSize(size, size);
+    static float const pi = std::acos(-1);
+    static float time = 0.0; 
+
+    time += elapsedSeconds;
+    size = m_size*std::abs(std::sin(2.0*pi*nu*time*0.001 + fi));
+   
+    m_sizeTmp = QSize(size, size);
   }
 
   QVector2D const & GetPosition() const { return m_position; }
 
-  QSize const & GetSize() const { return m_size; }
+  QSize const & GetSize() const { return m_sizeTmp; }
 private:
   QVector2D m_position;
-  QSize m_size;
-  unsigned char m_sizeTmp;
+  QSize m_sizeTmp;
+
+  float nu; //Hz
+  float fi; 
+  unsigned char m_size;
 };
